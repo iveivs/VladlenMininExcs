620b2a07c03ac3fb4929083ec64752779971b335
Svetoslav <veiv@mail.ru>
2023-04-02 03:38:11 +0300

    add 2 April

---
+++ Colors/app.js
@@ -0,0 +1,76 @@
+console.log('start');
+
+const cols = document.querySelectorAll('.col');
+
+document.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    if (event.code.toLowerCase() === 'space') {
+        setRundomColor();
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type;
+    if (type === 'lock') {
+        const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0]
+        node.classList.toggle('fa-lock-open');
+        node.classList.toggle('fa-lock');
+    } else if (type === 'copy') {
+        copyToClickboard(event.target.textContent)
+    }
+})
+
+function generateRandomColors() {
+    const hexCodes = '0123456789ABCDEF';
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+function copyToClickboard(text) {
+    return navigator.clipboard.writeText(text);
+}
+
+function setRundomColor() {
+    const colors = []
+    cols.forEach(col => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = generateRandomColors();
+
+        if (isLocked) {
+            colors.push(text.textContent)
+            return
+        }
+
+        colors.push(color);
+
+        text.textContent = color;
+        col.style.background = color;
+
+        setTextColor(text, color);
+        setTextColor(button, color)
+    })
+
+    updateColorsHash(colors)
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+
+function updateColorsHash(colors = []) {
+    document.location.hash = colors
+        .map((col) => {
+            return col.toString().substring(1)
+        })
+        .join('-')
+}
+
+setRundomColor();
